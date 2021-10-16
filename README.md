04ba22f83799f4d6bf34fa4ae14e987d93996068
Jonathan Ong <risingmidnight@ucla.edu>
2021-10-15 19:07:24 -0700

    Updated stuff

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # RestrictedPlanning
  
+Tasks so far
+
+using AWS
+Created EC2 instance on AWS (linux environment)
+Installed react on ec2 instance 52.53.151.5 -- is this needed??
+Created GitHub repo https://github.com/Jonathan805/RestrictedPlanning
+Set up aws amplify to auto deploy changes to the web: https://main.d1mnc2p6glgng0.amplifyapp.com/ -- Changes to the repo automatically deploy to the site
+set up certs (not necessary)
+Tasks to do
+
+drag and drop to assign aircraft, crew and weapons to targets -- figure out how to drag and drop
+build target info dock -- button to display weather -- button to display image -- show lat and long -- show name type etc. -- target info (ToT)
+Drag and drop ATO to initialize the view -- figure out file drag and drop capability -- populate things with javascript
+Panel/Menu to view current resources (ships, planes, pilots, weapons)
+"Lanes" to show times lines per target
+tabs to show timelines per player
--- restrictedplanning/src/Components/AtoInput.js
+++ restrictedplanning/src/Components/AtoInput.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react'
 
+
+//inspiration: https://medium.com/@650egor/simple-drag-and-drop-file-upload-in-react-2cb409d88929
 class AtoInput extends Component{
 
   dropRef = React.createRef()
 
-  componentDidMount(){
-    let div = this.dropRef.current
+  handleDrag = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+  handleDragIn = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+  handleDragOut = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+  handleDrop = (e) => {    
+    e.preventDefault()
+    e.stopPropagation()
   }
 
-  componentWillUnmount(){
+  componentDidMount() {
+    let div = this.dropRef.current
+    div.addEventListener('dragenter', this.handleDragIn)
+    div.addEventListener('dragleave', this.handleDragOut)
+    div.addEventListener('dragover', this.handleDrag)
+    div.addEventListener('drop', this.handleDrop)
+  }
 
+  componentWillUnmount() {
+    let div = this.dropRef.current
+    div.removeEventListener('dragenter', this.handleDragIn)
+    div.removeEventListener('dragleave', this.handleDragOut)
+    div.removeEventListener('dragover', this.handleDrag)
+    div.removeEventListener('drop', this.handleDrop)
   }
 
   render() {
