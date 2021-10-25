65b18a96a245857cfc822df2335d75d467ac5e4b
JGuzman333 <92654856+JGuzman333@users.noreply.github.com>
2021-10-24 18:22:13 -0700

    Updating style to delete target
    Updating style for target deletion

--- src/Components/Target.js
+++ src/Components/Target.js
@@ -3,7 +3,7 @@ import {FaTimes} from 'react-icons/fa'
 const Target = ({target}) => {
     return (
         <div className='target'>
-            <h3>{target.name} <FaTimes/></h3>
+            <h3>{target.name} <FaTimes style={{color:'red', cursor:'pointer'}}/></h3>
             <p>Lat:{target.latitude} Lon:{target.longitude} Elevation:{target.elevation} SuccessRate:{target.successRate}</p>
         </div>
     )
