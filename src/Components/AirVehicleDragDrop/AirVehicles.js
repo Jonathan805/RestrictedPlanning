c3e36abfce48b47a6f615a54d94fa4ad25f301af
JGuzman333 <92654856+JGuzman333@users.noreply.github.com>
2021-10-30 13:00:57 -0700

    Adding Helo
    Adding Helo in selectable aircraft

--- src/Components/AirVehicleDragDrop/AirVehicles.js
+++ src/Components/AirVehicleDragDrop/AirVehicles.js
@@ -8,6 +8,8 @@ import Growler from '../../AircraftImages/Growler.png';
 import FA18F from '../../AircraftImages/FA-18A.png';
 import F35 from '../../AircraftImages/F35.png';
 import B2 from '../../AircraftImages/B2.png';
+import AH64 from '../../AircraftImages/AH-64.png';
+
 
 const AirVehicles = () => {
     return (
@@ -21,9 +23,12 @@ const AirVehicles = () => {
           <Aircraft aircraftName='FA-18F (Hornet)' image={FA18F} />
         </Card>
         <Card id="Aircraft-3" className="aircraft" draggable="true">
-          <Aircraft aircraftName='F35' image={F35} />
+          <Aircraft aircraftName='AH64 (Apache)' image={AH64} />
         </Card>
         <Card id="Aircraft-4" className="aircraft" draggable="true">
+          <Aircraft aircraftName='F35' image={F35} />
+        </Card>
+        <Card id="Aircraft-5" className="aircraft" draggable="true">
           <Aircraft aircraftName='B2' image={B2} />
         </Card>
       </Board>
